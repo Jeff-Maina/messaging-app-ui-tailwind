553b32fd8932eb358177083368e902a6464c1acb
Jeff <jeffgichuki68@gmail.com>
2023-03-09 20:12:32 +0300

    chatbox setup

--- src/components/chatBox.js
+++ src/components/chatBox.js
@@ -1,11 +1,36 @@
-import "../styles/chatbox.css"
+import "../styles/chatbox.css";
 
 function Chatbox() {
-    return ( 
-        <section id="chatbox">
-
-        </section>
-     );
+  return (
+    <section id="chatbox">
+      <div id="nav">
+        <div id="contact-details">
+          <div id="contact-pfp"></div>
+          <div id="contact-user-name">
+            <h4>Max Chen</h4>
+            <h6>active</h6>
+          </div>
+        </div>
+        <div id="chat-options">
+          <i className="material-symbols-outlined">call</i>
+          <i className="material-symbols-outlined">video_call</i>
+          <i className="material-symbols-outlined">star</i>
+        </div>
+      </div>
+      <div className="convo-box"></div>
+      <div className="send-msg-row">
+        <div id="input-cont" >
+          <i className="material-symbols-outlined">sentiment_satisfied</i>
+          <input
+            type="text"
+            className="send-msg-input"
+            placeholder="Type a message"
+          />
+          <i className="material-symbols-outlined">attach_file</i>
+        </div>
+      </div>
+    </section>
+  );
 }
 
 export default Chatbox;
--- src/components/contacts.js
+++ src/components/contacts.js
@@ -1,142 +1,117 @@
-import "../styles/contacts.css"
+import "../styles/contacts.css";
 
 function Contacts() {
-    return ( 
-        <section id="contacts">
-            <div className="contacts-container">
-                 <div className="contact">
-                    <div className="contact-dp"></div>
-                    <div className="user-details">
-                        <h4>John Doe</h4>
-                        <p>
-                            Hey there! Just wanted to check in and see how you're doing. I hope you're having a great day so far. Let me know if you need anything or if you want to grab coffee sometime soon. Take care!"
-                        </p>
-                    </div>
-                 </div>
-                 <div className="contact">
-                    <div className="contact-dp"></div>
-                    <div className="user-details">
-                        <h4>John Doe</h4>
-                        <p>
-                            Hey there! Just wanted to check in and see how you're doing. I hope you're having a great day so far. Let me know if you need anything or if you want to grab coffee sometime soon. Take care!"
-                        </p>
-                    </div>
-                 </div>
-                 <div className="contact">
-                    <div className="contact-dp"></div>
-                    <div className="user-details">
-                        <h4>John Doe</h4>
-                        <p>
-                            Hey there! Just wanted to check in and see how you're doing. I hope you're having a great day so far. Let me know if you need anything or if you want to grab coffee sometime soon. Take care!"
-                        </p>
-                    </div>
-                 </div>
-                 <div className="contact">
-                    <div className="contact-dp"></div>
-                    <div className="user-details">
-                        <h4>John Doe</h4>
-                        <p>
-                            Hey there! Just wanted to check in and see how you're doing. I hope you're having a great day so far. Let me know if you need anything or if you want to grab coffee sometime soon. Take care!"
-                        </p>
-                    </div>
-                 </div>
-                 <div className="contact">
-                    <div className="contact-dp"></div>
-                    <div className="user-details">
-                        <h4>John Doe</h4>
-                        <p>
-                            Hey there! Just wanted to check in and see how you're doing. I hope you're having a great day so far. Let me know if you need anything or if you want to grab coffee sometime soon. Take care!"
-                        </p>
-                    </div>
-                 </div>
-                 <div className="contact">
-                    <div className="contact-dp"></div>
-                    <div className="user-details">
-                        <h4>John Doe</h4>
-                        <p>
-                            Hey there! Just wanted to check in and see how you're doing. I hope you're having a great day so far. Let me know if you need anything or if you want to grab coffee sometime soon. Take care!"
-                        </p>
-                    </div>
-                 </div>
-                 <div className="contact">
-                    <div className="contact-dp"></div>
-                    <div className="user-details">
-                        <h4>John Doe</h4>
-                        <p>
-                            Hey there! Just wanted to check in and see how you're doing. I hope you're having a great day so far. Let me know if you need anything or if you want to grab coffee sometime soon. Take care!"
-                        </p>
-                    </div>
-                 </div>
-                 <div className="contact">
-                    <div className="contact-dp"></div>
-                    <div className="user-details">
-                        <h4>John Doe</h4>
-                        <p>
-                            Hey there! Just wanted to check in and see how you're doing. I hope you're having a great day so far. Let me know if you need anything or if you want to grab coffee sometime soon. Take care!"
-                        </p>
-                    </div>
-                 </div>
-                 <div className="contact">
-                    <div className="contact-dp"></div>
-                    <div className="user-details">
-                        <h4>John Doe</h4>
-                        <p>
-                            Hey there! Just wanted to check in and see how you're doing. I hope you're having a great day so far. Let me know if you need anything or if you want to grab coffee sometime soon. Take care!"
-                        </p>
-                    </div>
-                 </div>
-                 <div className="contact">
-                    <div className="contact-dp"></div>
-                    <div className="user-details">
-                        <h4>John Doe</h4>
-                        <p>
-                            Hey there! Just wanted to check in and see how you're doing. I hope you're having a great day so far. Let me know if you need anything or if you want to grab coffee sometime soon. Take care!"
-                        </p>
-                    </div>
-                 </div>
-                 <div className="contact">
-                    <div className="contact-dp"></div>
-                    <div className="user-details">
-                        <h4>John Doe</h4>
-                        <p>
-                            Hey there! Just wanted to check in and see how you're doing. I hope you're having a great day so far. Let me know if you need anything or if you want to grab coffee sometime soon. Take care!"
-                        </p>
-                    </div>
-                 </div>
-                 <div className="contact">
-                    <div className="contact-dp"></div>
-                    <div className="user-details">
-                        <h4>John Doe</h4>
-                        <p>
-                            Hey there! Just wanted to check in and see how you're doing. I hope you're having a great day so far. Let me know if you need anything or if you want to grab coffee sometime soon. Take care!"
-                        </p>
-                    </div>
-                 </div>
-                 <div className="contact">
-                    <div className="contact-dp"></div>
-                    <div className="user-details">
-                        <h4>John Doe</h4>
-                        <p>
-                            Hey there! Just wanted to check in and see how you're doing. I hope you're having a great day so far. Let me know if you need anything or if you want to grab coffee sometime soon. Take care!"
-                        </p>
-                    </div>
-                 </div>
-                 <div className="contact">
-                    <div className="contact-dp"></div>
-                    <div className="user-details">
-                        <h4>John Doe</h4>
-                        <p>
-                            Hey there! Just wanted to check in and see how you're doing. I hope you're having a great day so far. Let me know if you need anything or if you want to grab coffee sometime soon. Take care!"
-                        </p>
-                    </div>
-                 </div>
+  return (
+    <section id="contacts">
+      <div className="contacts-container">
+        <div className="contact">
+          <div className="contact-dp"></div>
+          <div className="user-details">
+            <h4>John Doe</h4>
+            <p>
+              Hey there! Just wanted to check in and see how you're doing. I
+              hope you're having a great day so far. Let me know if you need
+              anything or if you want to grab coffee sometime soon. Take care!"
+            </p>
+          </div>
+        </div>
+        <div className="contact">
+          <div className="contact-dp"></div>
+          <div className="user-details">
+            <h4>Isaac Lee</h4>
+            <p>
+              Hey there! Just wanted to check in and see how you're doing. I
+              hope you're having a great day so far. Let me know if you need
+              anything or if you want to grab coffee sometime soon. Take care!"
+            </p>
+          </div>
+        </div>
+        <div className="contact">
+          <div className="contact-dp"></div>
+          <div className="user-details">
+            <h4>Samantha White</h4>
+            <p>
+              Hey there! Just wanted to check in and see how you're doing. I
+              hope you're having a great day so far. Let me know if you need
+              anything or if you want to grab coffee sometime soon. Take care!"
+            </p>
+          </div>
+        </div>
+        <div className="contact">
+          <div className="contact-dp"></div>
+          <div className="user-details">
+            <h4>Max Chen</h4>
+            <p>
+              Hey there! Just wanted to check in and see how you're doing. I
+              hope you're having a great day so far. Let me know if you need
+              anything or if you want to grab coffee sometime soon. Take care!"
+            </p>
+          </div>
+        </div>
+        <div className="contact">
+          <div className="contact-dp"></div>
+          <div className="user-details">
+            <h4>Olivia Martinez</h4>
+            <p>
+              Hey there! Just wanted to check in and see how you're doing. I
+              hope you're having a great day so far. Let me know if you need
+              anything or if you want to grab coffee sometime soon. Take care!"
+            </p>
+          </div>
+        </div>
+        <div className="contact">
+          <div className="contact-dp"></div>
+          <div className="user-details">
+            <h4>Ethan Davis</h4>
+            <p>
+              Hey there! Just wanted to check in and see how you're doing. I
+              hope you're having a great day so far. Let me know if you need
+              anything or if you want to grab coffee sometime soon. Take care!"
+            </p>
+          </div>
+        </div>
+        <div className="contact">
+          <div className="contact-dp"></div>
+          <div className="user-details">
+            <h4>Grace Kim</h4>
+            <p>
+              Hey there! Just wanted to check in and see how you're doing. I
+              hope you're having a great day so far. Let me know if you need
+              anything or if you want to grab coffee sometime soon. Take care!"
+            </p>
+          </div>
+        </div>
+        <div className="contact">
+          <div className="contact-dp"></div>
+          <div className="user-details">
+            <h4>Lucas Rodriguez</h4>
+            <p>
+              Hey there! Just wanted to check in and see how you're doing. I
+              hope you're having a great day so far. Let me know if you need
+              anything or if you want to grab coffee sometime soon. Take care!"
+            </p>
+          </div>
+        </div>
+        <div className="contact">
+          <div className="contact-dp"></div>
+          <div className="user-details">
+            <h4>Chloe Anderson</h4>
+            <p>
+              Hey there! Just wanted to check in and see how you're doing. I
+              hope you're having a great day so far. Let me know if you need
+              anything or if you want to grab coffee sometime soon. Take care!"
+            </p>
+          </div>
+        </div>
+        <div className="contact-last">
 
-            </div>
-            <div id="add-new-contact">
-                <i className="material-icons">add</i>
-            </div>
-        </section>
-     );
+        </div>
+      </div>
+      <div id="add-new-contact">
+        <i className="material-icons">add</i>
+      </div>
+    </section>
+  );
 }
 
 export default Contacts;
